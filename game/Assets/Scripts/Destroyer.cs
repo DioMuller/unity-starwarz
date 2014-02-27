68d44fe1bead07a65208f6a9be7f605e227f1574
Diogo Muller <diogo.muller@live.com>
2014-02-27 12:52:00 -0300

    Now the Explosion prefab autodestroys the gameObject after a set time.

---
+++ game/Assets/Scripts/Destroyer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyer : MonoBehaviour 
+{
+	public float WaitTime = 3.0f;
+
+	// Use this for initialization
+	void Awake () 
+	{
+		StartCoroutine(DestroyAfterDelay());	
+	}
+	
+	private IEnumerator DestroyAfterDelay() 
+	{
+		yield return new WaitForSeconds(WaitTime);
+		Destroy(gameObject);
+	}
+}
