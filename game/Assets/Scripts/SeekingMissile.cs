c4061d6ed6b9dc398999d7bb4be174745830c1d2
Diogo Muller <diogo.muller@live.com>
2014-03-21 19:17:48 -0300

    Seeking Missile has the option (disabled by default) to follow a single target.

--- game/Assets/Scripts/SeekingMissile.cs
+++ game/Assets/Scripts/SeekingMissile.cs
@@ -12,25 +12,48 @@ public class SeekingMissile : MonoBehaviour
 	/// The rotation speed (Euler).
 	/// </summary>
 	public float rotateSpeed;
+
+	/// <summary>
+	/// Will the missile seek a fixed target? (The last spawned
+	/// item after awake), or always the last spawn item?
+	/// </summary>
+	public bool fixedTarget = false;
 	#endregion Public Attributes
 
+	#region Private Attributes
+	public Transform target = null; 
+	#endregion Private Attributes
+
 	#region MonoBehaviour Methods
+	/// <summary>
+	/// Initializes the target, if it is fixed.
+	/// </summary>
+	void Awake ()
+	{
+		if( !fixedTarget || Game.Data.LatestNPCs.Count == 0 ) return;
+
+		target = Game.Data.LatestNPCs[0];
+	}
+
 	/// <summary>
 	/// Rotates the object to the target's position.
 	/// </summary>
 	void Update ()
 	{
-		if (Game.Data.LatestNPCs.Count != 0) 
-		{
-			var dT = Time.deltaTime;
+		if (Game.Data.LatestNPCs.Count == 0) return; // No target to follow.
 
-			var current = transform.rotation;
-			transform.LookAt (Game.Data.LatestNPCs[0]);
-			var rotated = transform.rotation;
+		if( !fixedTarget ) target = Game.Data.LatestNPCs[0]; // Not Fixed, will update with the newest.
 
-			transform.rotation = Quaternion.RotateTowards (
-			current, rotated, rotateSpeed * dT);
-		}
+		if( target == null ) return; // Was Destroyed.
+
+		var dT = Time.deltaTime;
+
+		var current = transform.rotation; // Stores Current location
+		transform.LookAt (target); // Obtains desired location
+		var rotated = transform.rotation; // Stores desired location
+
+		// Rotates Towards the desired location.
+		transform.rotation = Quaternion.RotateTowards (current, rotated, rotateSpeed * dT);
 	}
 	#endregion MonoBehaviour Methods
 }
