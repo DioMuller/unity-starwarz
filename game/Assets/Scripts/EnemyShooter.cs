d8044e71b965420a52c6fed0875071beec3f4252
Diogo Muller <diogo.muller@lactec.org.br>
2014-03-21 09:15:13 -0300

    Added enemy shot.

---
+++ game/Assets/Scripts/EnemyShooter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooter : MonoBehaviour 
+{
+	public Transform EnemyShot;
+	public Transform EnemySpawnPosition;
+	public float ReloadTime = 3.0f;
+	
+	void Awake() 
+	{
+		// First shot will be fired in a random time between 0 and Reload Time.
+		float start = Random.Range(0.0f, ReloadTime);
+
+		InvokeRepeating("Shoot", start, ReloadTime);
+	}
+	
+	private void Shoot() 
+	{
+        Instantiate(EnemyShot, EnemySpawnPosition.position, Quaternion.identity);
+	}
+}
--- game/Assets/Scripts/PlasmaShot.cs
+++ game/Assets/Scripts/PlasmaShot.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 
 public class PlasmaShot : MonoBehaviour {
-	
+	public string TargetTag = "NPC";
 	// Update is called once per frame
 	void OnTriggerEnter (Collider obstacle) {
 		string tag = obstacle.gameObject.tag;
-		if (tag == "NPC") 
+		if ( tag == TargetTag ) 
         {
 			obstacle.SendMessage(Game.Data.WasHitMsg);
 			DestroySelf();
